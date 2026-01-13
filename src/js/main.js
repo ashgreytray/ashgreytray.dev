fcdfc0db0693b34cba94337030f689c4468dd47c
ashgreytray <asche1.grey@gmail.com>
2026-01-14 04:13:06 +1000

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -34,51 +34,28 @@ musicTab.addEventListener('click', async () => {
 	}
 });
 
-const track = document.getElementById("image-track");
+const art = document.getElementById('art');
+const trackName = document.getElementById('trackName');
+const artistName = document.getElementById('artistName');
+const albumName = document.getElementById('albumName');
+const eq = document.getElementById('eq');
 
-const handleOnDown = e => track.dataset.mouseDownAt = e.clientX;
+art.src = track.image[large][t];
+trackName.innerText = track.name;
+artistName.innerText = track.artist[t];
+albumName.innerText = track.album[t];
 
-const handleOnUp = () => {
-  track.dataset.mouseDownAt = "0";  
-  track.dataset.prevPercentage = track.dataset.percentage;
+loading.classList.add('hidden');
+
+// now playing indicator
+if (track['@attr'] && track['@attr'].nowplaying) {
+    playTitle.innerText = 'Now Playing';
+    eq.style.display = 'flex';
+} else {
+    playTitle.innerText = 'Last Played';
+    eq.style.display = 'none';
 }
 
-const handleOnMove = e => {
-  if(track.dataset.mouseDownAt === "0") return;
-  
-  const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX,
-        maxDelta = window.innerWidth / 2;
-  
-  const percentage = (mouseDelta / maxDelta) * -100,
-        nextPercentageUnconstrained = parseFloat(track.dataset.prevPercentage) + percentage,
-        nextPercentage = Math.max(Math.min(nextPercentageUnconstrained, 0), -100);
-  
-  track.dataset.percentage = nextPercentage;
-  
-  track.animate({
-    transform: `translate(${nextPercentage}%, -50%)`
-  }, { duration: 1200, fill: "forwards" });
-  
-  for(const image of track.getElementsByClassName("image")) {
-    image.animate({
-      objectPosition: `${100 + nextPercentage}% center`
-    }, { duration: 1200, fill: "forwards" });
-  }
-}
-
-/* -- Had to add extra lines for touch events -- */
-
-window.onmousedown = e => handleOnDown(e);
-
-window.ontouchstart = e => handleOnDown(e.touches[0]);
-
-window.onmouseup = e => handleOnUp(e);
-
-window.ontouchend = e => handleOnUp(e.touches[0]);
-
-window.onmousemove = e => handleOnMove(e);
-
-window.ontouchmove = e => handleOnMove(e.touches[0]);
 
 document.addEventListener('DOMContentLoaded', () => {
 	addThemeList();
@@ -108,4 +85,5 @@ document.addEventListener('DOMContentLoaded', () => {
 		document.getElementById('grunge-overlay').style.display = 'none';
 	}
 });
+
 
